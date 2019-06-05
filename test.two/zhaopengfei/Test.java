cd208deab0f406effa611c00b1c6e35dd53dddb5
zhangqingUU <852378999@qq.com>
2019-06-05 16:34:32 +0800

    张晴的hehe()

---
+++ test.two/zhaopengfei/Test.java
@@ -0,0 +1,11 @@
+package zhaopengfei;
+
+import cn.kgc.tangcco.zhangqing.service.PersonService;
+import cn.kgc.tangcco.zhangqing.service.impl.PersonServiceImpl;
+
+public class Test {
+	public static void main(String[] args) {
+		PersonService ps=new PersonServiceImpl();
+		ps.hehe();
+	}
+}
